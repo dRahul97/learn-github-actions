83e249496cc96fcac65414337e433b1551f55c18
RahulDudka <rahuldudka27@gmail.com>
2023-01-09 00:33:50 +0530

    index file updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <p>Hello there</p>;
+function Home({ data }) {
+  return (
+    <div>
+      <h1>HomePages</h1>
+
+      {console.log(data)}
+    </div>
+  );
 }
+
+export async function getServerSideProps() {
+  let res = await fetch(
+    `https://api.github.com/search/repositories?q=dRahul97`
+  );
+
+  let data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+export default Home;
